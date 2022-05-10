d92a26e0c2ddb9c2c852879890b88b2404ff9c2e
dmytro_mykoliv <amigo.del.ua@gmail.com>
2022-05-11 00:34:39 +0300

    fixed photo

--- src/app/mock-img-for-game.ts
+++ src/app/mock-img-for-game.ts
@@ -1,10 +1,15 @@
+// import * as path from 'path';
+
+// console.log(__dirname);
+// console.log(path.resolve());
+
 export const PICTURES = [
-  "../../assets/images/oleg.jpg",
-  "../../assets/images/me.jpg",
-  "../../assets/images/penguin.jpg",
-  "../../assets/images/burbn.jpg",
-  "../../assets/images/ass.jpg",
-  "../../assets/images/food.jpg",
-  "../../assets/images/ivan.jpg",
-  "../../assets/images/vovik.jpg"
+  "./assets/images/oleg.jpg",
+  "./assets/images/me.jpg",
+  "./assets/images/penguin.jpg",
+  "./assets/images/burbn.jpg",
+  "./assets/images/ass.jpg",
+  "./assets/images/food.jpg",
+  "./assets/images/ivan.jpg",
+  "./assets/images/vovik.jpg"
 ]
